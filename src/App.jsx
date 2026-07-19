e193d0d50301e62c2339879a192db2b9c8629849
KAVANA MANJUNATH HEGDE <kavanah06@gmail.com>
2026-07-19 13:48:23 -0700

    Implement useState and TodoListItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 import TodoList from "./TodoList.jsx";
 import TodoForm from "./TodoForm.jsx";
 
+const todos = [
+  { id: 1, title: "Review resources" },
+  { id: 2, title: "Take notes" },
+  { id: 3, title: "Code out app" },
+];
+
 function App() {
+  const [todoList, setTodoList] = useState(todos);
+
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm />
-      <TodoList />
+      <TodoList todoList={todoList} />
     </div>
   );
 }
